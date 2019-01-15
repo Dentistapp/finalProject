d04e5aaf4c48c27166e95dfe75d6961664c10468
Itzel Gomez <gomi1901@gmail.com>
2019-01-15 01:21:59 -0600

    Add button Facebook

--- dientitosApp/dientitosApp/Controllers/LoginViewController.swift
+++ dientitosApp/dientitosApp/Controllers/LoginViewController.swift
@@ -19,6 +19,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
 
         // Do any additional setup after loading the view.
     }
@@ -38,5 +39,26 @@ class LoginViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    
+    @objc func facebookButtonAction(sender: UIButton!) {
+        print("Facebook login")
+    }
+    
+    //MARK: Setup Button Facebook
+    
+    fileprivate func setupViews(){
+       
+        let buttonFacebook = UIButton(frame: CGRect(x: 62, y: 550, width: 300, height: 30))
+        buttonFacebook.backgroundColor = UIColor(named: "facebook")
+        buttonFacebook.setImage(UIImage(named: "facebookLogo")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        buttonFacebook.setTitle("Sign in with Facebook", for: .normal)
+        buttonFacebook.addTarget(self, action: #selector(facebookButtonAction), for: .touchUpInside)
+        self.view.addSubview(buttonFacebook)
+
+
+}
+
 
 }
